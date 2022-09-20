036d9f509a664dc5ddaf2989852601df73794b79
UNIWYLIMP <77296707+UNIWYLIMP@users.noreply.github.com>
2022-09-21 00:56:59 +0100

    Update 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
+    new_string = ""
     for i in str:
-        if ord(i) >= ord('a') and ord(i) <= ord('z'):
-            i = chr(ord(i) - 32)
-    print("{:s}".format(i), end="")
-    print()
+        if ord(i) < 97: 
+            new_string += i
+        else:
+            new_variant = ord(i) - 32
+            new_string += chr(new_variant)
+    print("{:s}".format(new_string), end="")
